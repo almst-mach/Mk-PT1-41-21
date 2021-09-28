b945db5bd6611d613fdb4055cd2a6384405fa6c3
Mikita Tsiarentsyeu <Mikita_Tsiarentsyeu@epam.com>
2021-09-28 21:20:59 +0300

    Add lesson 28.09

---
+++ Lessons/28.09.21/io.py
@@ -0,0 +1,20 @@
+print("some text")
+
+x = "another text"
+print(x)
+
+y = 4
+z = 5.9
+
+print(y + z)
+
+print(x, y, z)
+
+print(x, y, z, sep=', ', end="\nthis is the end,\nI'm sure of it, \nreally\n")
+#print("", end='\n')
+print(x)
+
+#i = int(input())
+i = input("Please input your data:\n")
+print(i)
+print(type(i))
