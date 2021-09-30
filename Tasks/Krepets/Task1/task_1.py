c199559213b031b5529cd3b461a22e16dc73ba77
Anton Krepets <krepets02@mail.ru>
2021-09-30 19:04:02 +0300

    Update task_1.py

--- Tasks/Krepets/Task1/task_1.py
+++ Tasks/Krepets/Task1/task_1.py
@@ -3,7 +3,7 @@ def credit():
     term = int(input('Enter term(in months): '))
     percent = float(input('Enter annual percentage: '))
     end_sum = start_sum * ((1 + (percent / 100)/12)**term)
-    print(f'Amount is {end_sum}')
+    print('Amount is %.2f' % end_sum)
 
 
 credit()
@@ -12,4 +12,4 @@ x = 20000
 y = 60
 c = 15
 sum_2 = x * ((1 + (c / 100)/12)**y)
-print(sum_2)
+print('%.2f' % sum_2)
