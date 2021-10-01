629441946de87b72282c56cad8ade208359a103c
VolhaKurch <91286271+VolhaKurch@users.noreply.github.com>
2021-10-01 19:13:39 +0300

    delete

---
+++ Tasks/kurchevskaya/task1/30.py
@@ -0,0 +1,10 @@
+x=-5
+if x==0:
+    print("it's zero")
+elif x==2:
+    print("it's two")
+else:
+    print("none")
+
+print("good") if x>0 else print("bad")
+#print("good" if x>0 else print("bad")
