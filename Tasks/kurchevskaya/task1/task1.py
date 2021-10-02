46de8127175a0f866d032f81198974ff9fa171ef
VolhaKurch <91286271+VolhaKurch@users.noreply.github.com>
2021-10-02 18:05:02 +0300

    Update task1.py

--- Tasks/kurchevskaya/task1/task1.py
+++ Tasks/kurchevskaya/task1/task1.py
@@ -1,14 +1,11 @@
 from decimal import *
 i_deposit = input("ВВедите размер начального депозита:")
-i_deposit= int(i_deposit)
+i_deposit= Decimal(i_deposit)
 percent = input("Введите размер процента годовой:")
-percent = int(percent)/100
-#percent = 15/100
-#срок договора в месяцах
+percent = Decimal(percent)/100
 t=input("Введите срок депозита в годах:")
-t=int(t)*12
-#t = 12*5
-#income = i_deposit*(1+percent/12)**t
+t=Decimal(t)*12
+
 income = Decimal(str(i_deposit*(1+percent/12)**t))
 rounded = income.quantize(Decimal('0.01'), rounding = ROUND_UP)
 print(rounded)
