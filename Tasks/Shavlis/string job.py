de2b80cfa11d9a6a862fdcce986809e825ac7f86
mishashavlis <mixas314@gmail.com>
2021-10-13 11:30:08 +0300

    Update string job.py

---
+++ Tasks/Shavlis/string job.py
@@ -0,0 +1,17 @@
+l = 'five thirteen two eleven seventeen two one thirteen ten four eight five nineteen'
+l = l.split(' ')
+a = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9, 'ten':10, 'eleven':11, 'twelve':12, 'thirteen':13, 'fourteen':14, 'fifteen':15, 'sixteen':16, 'seventeen':17, 'eighteen':18, 'nineteen':19, 'twenty':20}
+b = []
+for i in l:
+    b.append(a[i])
+print(b)
+print(l)
+
+c = []
+for i in range(len(b)):
+    for j in range(len(b)):
+        if j == len(b)-1:
+            continue
+        if l[j]>l[j+1] or l[j]<l[j+1]:
+            c.append(b[j])
+print(c)
