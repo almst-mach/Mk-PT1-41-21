5cb81501cfd014e08a0d4a7ab1bd5f93f6b8cee6
mishashavlis <mixas314@gmail.com>
2021-10-21 23:45:35 +0300

    Update sort_list.py

--- Tasks/Shavlis/Lessons/sort_list.py
+++ Tasks/Shavlis/Lessons/sort_list.py
@@ -1,7 +1,24 @@
 import random
+
 l = [4,3,6,5,7,9,2]
 
-def random_item():
-    random.randrange(len(l))
-    print()
-print(random_item())
+def sorting(l):
+    return l == sorted(l)
+
+def random_item(lenght):
+    x = random.randint(0, lenght-1)
+    y = random.randint(0, lenght-1)
+    while x == y:
+        y = random.randint(0, lenght-1)
+    return x, y
+
+def sort(l):
+    counter = 0
+    while not sorting(l):
+        counter += 1
+        x, y = random_item(len(l))
+        l[x], l[y] = l[y], l[x]
+    print(l)
+    return counter
+
+print(sort(l))
