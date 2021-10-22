6efb4398590c3c681cd44476b27385d91e3160bf
mishashavlis <mixas314@gmail.com>
2021-10-22 11:01:09 +0300

    read

--- Tasks/Shavlis/working with a file.py
+++ Tasks/Shavlis/working with a file.py
@@ -1,7 +1,9 @@
-with open(r'C:\python\Mk-PT1-41-21\Tasks\Shavlis\text.txt', 'r', encoding = 'UTF-8' ) as f:
-    for i in f:
-        x = 0
-        x = f.read(x)
-        x = x + 40
-    print (x)
-        
+b = input('Количество символов ')
+
+with open(r'C:\python\Mk-PT1-41-21\Tasks\Shavlis\text.txt', encoding = 'UTF-8') as f:
+    a = f.read(int(b))
+    
+with open(r'C:\python\Mk-PT1-41-21\Tasks\Shavlis\text2.txt', 'w', encoding = 'UTF-8') as f:
+    f.write(a)
+
+print(f)     
