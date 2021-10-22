28529b48db6c44b1be69d5bf8b02f915505d7942
mishashavlis <mixas314@gmail.com>
2021-10-22 08:27:27 +0300

    go

---
+++ Tasks/Shavlis/working with a file.py
@@ -0,0 +1,7 @@
+with open(r'C:\python\Mk-PT1-41-21\Tasks\Shavlis\text.txt', 'r', encoding = 'UTF-8' ) as f:
+    for i in f:
+        x = 0
+        x = f.read(x)
+        x = x + 40
+    print (x)
+        
